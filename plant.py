8d6b3630877fd43d209339e6a6850ab2ee4e8673
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2024-02-17 23:15:02 -0500

    idrk

--- plant.py
+++ plant.py
@@ -1,11 +1,12 @@
 import cohere
-from documents import Documents, Chatbot
+from typing import List, Dict
 
 class Plant:
     type: str
     preamble: str
+    documents: List[Dict[str, str]]
 
-    def plant_chat(self, prompt) -> str:
+    def plant_msg(self, prompt) -> str:
         co = cohere.Client('GbLhM3APFZEtc07r1T6HHIXj6H3JYVevrNudGhNc')
     
         response = co.chat(
@@ -13,8 +14,8 @@ class Plant:
             message=prompt,
             temperature=0.6,
             preamble_override=self.preamble,
-            connectors=[{"id": "web-search"}],
-            max_tokens=50
+            documents=self.documents,
+            max_tokens=100
         )
 
         print(response.text)
@@ -23,11 +24,26 @@ class Plant:
 class Cactus(Plant):
     type = "cactus"
     preamble = "You are an old, grumpy, tough-love grandfather cactus."
+    documents = [
+        {"title": "Tall penguins", "snippet": "Emperor penguins are the tallest."},
+        {"title": "Penguin habitats", "snippet": "Emperor penguins only live in Antarctica."},
+        {"title": "What are animals?", "snippet": "Animals are different from plants."}
+    ]
 
 class Bonsai(Plant):
     type = "bonsai"
     preamble = "You are a wise, patient, and tranquil elder bonsai."
-
+    documents = [
+        {"title": "Procrastination", "snippet": "Procrastination is good."},
+        {"title": "Penguin habitats", "snippet": "Emperor penguins only live in Antarctica."},
+        {"title": "What are animals?", "snippet": "Animals are different from plants."}
+    ]
+    
 class Dandelion(Plant):
     type = "dandelion"
     preamble = "You are a cheerful dandelion, bursting with youthful energy."
+    documents = [
+        {"title": "Tall penguins", "snippet": "Emperor penguins are the tallest."},
+        {"title": "Penguin habitats", "snippet": "Emperor penguins only live in Antarctica."},
+        {"title": "What are animals?", "snippet": "Animals are different from plants."}
+    ]
